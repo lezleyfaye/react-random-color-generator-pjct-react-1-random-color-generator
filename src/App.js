b31bd86adb6e292292a76478d81894ec4358dac6
Lezley Faye <lezleydavidson@gmail.com>
2023-01-17 18:46:08 +0100

    Update useState string to empty

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 
 export default function App() {
-  const [hex, setHex] = useState('#ffffff');
+  const [hex, setHex] = useState('');
   const generateRandomColor = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
