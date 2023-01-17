a1bc04d567f237267c5e41dfe13d28362cedf13a
Lezley Faye <lezleydavidson@gmail.com>
2023-01-17 16:02:37 +0100

    Add color generator base code

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Random Color Generator
+
+A random color generator created in React that will:
+
+-show a random color when the generate button is clicked
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState } from 'react';
+
+export default function App() {
+  const [hex, setHex] = useState('#ffffff');
+  const generateRandomColor = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    setHex(randomColor);
+  };
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: `${hex}`,
+        overflow: 'hidden',
+        zoom: '2',
+      }}
+    >
+      <div>
+        <h1>Generated color: {hex}</h1>
+      </div>
+      <button onClick={generateRandomColor}>Generate</button>
+    </div>
+  );
+}
